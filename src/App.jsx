60db6f6dbca5b14dc0c7411cadc40902c605af0b
Anvarbek <ziyodovanvar1999@gmail.com>
2025-04-17 13:44:29 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-red-500 text-4xl">
+      This is the setup file for vite{" "}
+    </div>
+  );
+}
+
+export default App;
