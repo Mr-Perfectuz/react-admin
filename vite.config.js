024043cebd3e1b6ac88b491830d2e15e3907cb6a
Anvarbek <ziyodovanvar1999@gmail.com>
2025-04-18 16:04:11 +0900

    Fix: error for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[hash][extname]",
-        chunkFileNames: "assets/[name].[hash].js",
-        entryFileNames: "assets/[name].[hash].js",
-      },
-    },
-  },
   plugins: [react()],
 });
