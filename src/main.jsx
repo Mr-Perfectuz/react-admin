7930819ab18bf90500c91e833ecc2666b4cb9693
Anvarbek <ziyodovanvar1999@gmail.com>
2025-04-18 15:49:02 +0900

    Fix: error

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-const isProduction = import.meta.env.PROD;
-const baseName = isProduction ? "/react-admin" : "";
+const baseName = "/react-admin";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/react-admin/" : "/",
+  base: "/react-admin/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
   plugins: [react()],
 });
